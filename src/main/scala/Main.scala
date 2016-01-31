c40e76cc53eb6254d2934ccff62ee065c44fa3f5
sinaghaffari <sina.ghaffari321@gmail.com>
2016-01-30 22:00:46 -0500

    Fixed many problems and finished cleaning up code.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -30,93 +30,59 @@ object Main extends App with SimpleRoutingApp {
 
   val route =
     path("upvote") {
-      get {
-        clientIP { ip =>
-          println(s"$ip sent request.")
-          parameters('user1, 'user2) { (_user1, _user2) => ctx: RequestContext =>
+      get { ctx: RequestContext =>
+        parameters('user1.as[String], 'user2.as[String]) { (_user1, _user2) =>
+          clientIP { ip =>
+            println(s"$ip sent request.")
             println(s"Request: $ctx")
             val user1 = _user1.toString.toLowerCase
             val user2 = _user2.toString.toLowerCase
-            isUserInvalid(user1)
-            .fallbackTo(isUserInvalid(user2))
-            .fallbackTo(isNotNightbot(ip))
-            .fallbackTo(isSelfVote(user1, user2))
-            .fallbackTo(cantVote(user1, user2))
-            .fallbackTo(
-              Try(voteModel.insertVote(Vote(UUID.randomUUID, user1, user2, DateTime.now, 1))) match {
-                case Success(s) => getKarma(user2).map( karma => s"$user1 upvoted $user2. $karma")
-                case Failure(f) => Future("Failure")
-              }).map(message => ctx.complete(message))
+            complete(
+              isUserInvalid(user1)
+                .fallbackTo(isUserInvalid(user2))
+                .fallbackTo(isNotNightbot(ip))
+                .fallbackTo(isSelfVote(user1, user2))
+                .fallbackTo(cantVote(user1, user2))
+                .fallbackTo(
+                  Try(voteModel.insertVote(Vote(UUID.randomUUID, user1, user2, DateTime.now, 1))) match {
+                    case Success(s) => getKarma(user2).map(karma => s"$user1 upvoted $user2. $karma")
+                    case Failure(f) => Future("Failure")
+                  })
+            )
           }
         }
       }
     } ~
     path("downvote") {
-      get {
-        clientIP { ip =>
-          println(ip.toString)
-          parameters('user1.as[String], 'user2.as[String]) { (_user1, _user2) => ctx: RequestContext =>
+      get { ctx: RequestContext =>
+        parameters('user1.as[String], 'user2.as[String]) { (_user1, _user2) =>
+          clientIP { ip =>
+            println(s"$ip sent request.")
+            println(s"Request: $ctx")
             val user1 = _user1.toString.toLowerCase
             val user2 = _user2.toString.toLowerCase
-
-            pipeline(Get(s"https://api.twitch.tv/kraken/channels/$user1")).map { user1Response =>
-              pipeline(Get(s"https://api.twitch.tv/kraken/channels/$user2")).map { user2Response =>
-                if (user1Response.status.isSuccess && user2Response.status.isSuccess) {
-                  if (ip.toOption.get.getHostAddress.startsWith("23.92.68") || ip.toOption.get.getHostAddress.startsWith("107.155.125")) {
-                    if (user1.toString.toLowerCase == user2.toString.toLowerCase) {
-                      ctx.complete("You cannot vote on yourself.")
-                    } else {
-                      voteModel.findSentVotes(user1).map { sentVotes =>
-                        val last10 = sentVotes.take(10)
-                        println("last10: " + last10)
-                        val voteMap = last10.map(_.user2).distinct.map(v => v -> last10.filter(_.user2 == v).map(_.time)).toMap
-                        println("voteMap: " + voteMap)
-                        var credits = 10.0
-                        if (voteMap.nonEmpty)
-                          credits += (DateTime.now.getMillis - last10.head.time.getMillis).millisecond.toMinutes
-
-                        voteMap.foreach { usersVotedOn =>
-                          println("usersVotedOn: " + usersVotedOn)
-                          credits -= ((1 - Math.pow(1.25, usersVotedOn._2.length)) / (1 - 1.25))
-                        }
-                        println("credits: " + credits)
-                        var allocation = credits
-                        if (voteMap.contains(user2))
-                          allocation -= Math.pow(1.25, voteMap(user2).length + 1)
-                        if (allocation <= 0) {
-                          ctx.complete("You have voted too many times recently. Please wait and try again.")
-                        } else {
-                          Try(voteModel.insertVote(Vote(UUID.randomUUID, user1.toString.toLowerCase, user2.toString.toLowerCase, DateTime.now, -1))) match {
-                            case Success(s) => ctx.complete(s"$user1 downvoted $user2. ${getKarma(user2)}")
-                            case Failure(f) => ctx.complete(s"Failed to downvote user.")
-                          }
-                        }
-                      }
-                    }
-                  } else {
-                    ctx.complete(s"Nice try, you can't cheat the system.")
-                  }
-                } else {
-                  ctx.complete(s"One of the users entered does not exist.")
-                }
-              }
-            }
+            complete(
+              isUserInvalid(user1)
+                .fallbackTo(isUserInvalid(user2))
+                .fallbackTo(isNotNightbot(ip))
+                .fallbackTo(isSelfVote(user1, user2))
+                .fallbackTo(cantVote(user1, user2))
+                .fallbackTo(
+                  Try(voteModel.insertVote(Vote(UUID.randomUUID, user1, user2, DateTime.now, -1))) match {
+                    case Success(s) => getKarma(user2).map(karma => s"$user1 downvoted $user2. $karma")
+                    case Failure(f) => Future("Failure")
+                  }))
           }
         }
       }
     } ~
     path("karma") {
-      get {
-        clientIP { ip =>
-          parameter('user) { _user => ctx: RequestContext =>
+      get { ctx: RequestContext =>
+        parameters('user.as[String]) { _user =>
+          clientIP { ip =>
             val user = _user.toLowerCase
-            pipeline(Get(s"https://api.twitch.tv/kraken/channels/$user")).map { userResponse =>
-              if (userResponse.status.isSuccess) {
-                ctx.complete(getKarma(user))
-              } else {
-                ctx.complete(s"That user does not exist.")
-              }
-            }
+            complete(isUserInvalid(user)
+              .fallbackTo(getKarma(user)))
           }
         }
       }
@@ -125,6 +91,7 @@ object Main extends App with SimpleRoutingApp {
   startServer(interface = "159.203.16.72", port = 8080) {
     route
   }
+
   def isUserInvalid(user: String): Future[String] = {
     pipeline(Get(s"https://api.twitch.tv/kraken/channels/$user")).map { response =>
       if (response.status.isFailure) {
